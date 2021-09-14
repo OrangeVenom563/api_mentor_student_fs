67754ff8eedd3dd45b6b782311b85dbff0ad50e8
Lawrence J <lawrenceg563@gmail.com>
2021-09-15 05:05:54 +0530

    created mvc folder pattern and error handler controller

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
 
+
 app.get('/',(req,res)=>{
     res.send('Hello from express')
 })
+
+app.use(require('./controller/error'));
 
 app.listen(3000,()=>console.log('server listerning to port 3000'))
